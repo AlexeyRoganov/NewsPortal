d821a28ff183382939a4fa732793ee6acb2158ec
Роганов Алексей <jrv73@rambler.ru>
2022-03-12 10:46:41 +0300

    Initial commit

--- portal/forms.py
+++ portal/forms.py
@@ -1,6 +1,7 @@
 from django.forms import ModelForm, BooleanField
 from .models import Post
-
+from allauth.account.forms import SignupForm
+from django.contrib.auth.models import Group
 
 # Создаём модельную форму
 class ProductForm(ModelForm):
@@ -8,3 +9,12 @@ class ProductForm(ModelForm):
     class Meta:
         model = Post
         fields = ['author', 'title', 'text', 'cats', 'check_box']
+
+
+class BasicSignupForm(SignupForm):
+
+    def save(self, request):
+        user = super(BasicSignupForm, self).save(request)
+        basic_group = Group.objects.get(name='common')
+        basic_group.user_set.add(user)
+        return user
